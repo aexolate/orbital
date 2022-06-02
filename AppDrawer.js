60db0838182d8ad12f98c330428f5954c01ab733
nus-jeff <e0727134@u.nus.edu>
2022-06-02 15:02:45 +0800

    added menu features

---
+++ AppDrawer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import MapMenu from './src/Menu/MapMenu'; 
+import SettingsMenu from './src/Menu/SettingsMenu';
+
+const Drawer = createDrawerNavigator();
+ 
+const AppDrawer = () => {
+  return (
+    <Drawer.Navigator useLegacyImplementation>
+      <Drawer.Screen name="Map" component={MapMenu} />
+      <Drawer.Screen name="Settings" component={SettingsMenu} />
+    </Drawer.Navigator>
+  );
+}
+ 
+export default AppDrawer;
