624c7d1d11fb92fd82b74cfe7ee3f38d94b5e119
nus-jeff <e0727134@u.nus.edu>
2022-07-05 17:30:26 +0800

    added permissions method for customAudio, WIP

--- src/Menu/SettingsMenu.js
+++ src/Menu/SettingsMenu.js
@@ -5,23 +5,28 @@ import { getData, storeData } from '../utils/AsyncStorage';
 import PropTypes from 'prop-types';
 import * as MediaLibrary from 'expo-media-library';
 
+//TO FIX FOR HANDLING ALL CONDITIONS
 const getPermission = async () => {
   const permission = await MediaLibrary.getPermissionsAsync();
   console.log(permission);
-};
+  if (!permission.granted && permission.canAskAgain) {
+    const { status, canAskAgain } = await MediaLibrary.requestPermissionsAsync();
+  }
 
+};
 //activation radius is currently only set in confirm location, should change to on load screen
 const SettingsMenu = () => {
   const DEFAULT_RADIUS = 500;
   const [radiusText, setRadiusText] = useState(''); //text for radius setting input
   const [radiusValue, setRadiusValue] = useState(DEFAULT_RADIUS); //radius that is displayed in app, also the current setting value
-  
+
   useEffect(() => {
     getData('radius').then((radius) => {
-      getPermission();
+      getPermission(); //maybe place in select button for custom instead
       setRadiusValue(radius == null ? DEFAULT_RADIUS : radius);
     });
   }, []);
+
 
   const SettingsButton = (props) => {
     return (
