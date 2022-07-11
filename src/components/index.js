09303984dad82cc82187eb653de868c1bbda8bf4
aexolate <20797128+aexolate@users.noreply.github.com>
2022-07-11 15:16:36 +0800

    Deprecate unused components

--- src/components/index.js
+++ src/components/index.js
@@ -3,10 +3,10 @@ import FavouritesDialog from './FavouritesDialog';
 import InfoBox from './InfoBox';
 import PromptBox from './PromptBox';
 import SearchbarLocation from './SearchbarLocation';
-import SnackbarHint from './SnackbarHint';
 import WaypointIndicator from './WaypointIndicator';
 import MenuButton from './MenuButton';
 import WaypointsModal from './WaypointsModal';
+import RadiusTextInput from './RadiusTextInput';
 
 export {
   AlarmBox,
@@ -14,8 +14,8 @@ export {
   InfoBox,
   PromptBox,
   SearchbarLocation,
-  SnackbarHint,
   WaypointIndicator,
   MenuButton,
   WaypointsModal,
+  RadiusTextInput,
 };
--- src/screens/PermissionsScreen.tsx
+++ src/screens/PermissionsScreen.tsx
@@ -1,6 +1,6 @@
 import * as Location from 'expo-location';
 import { Linking, StyleSheet, View } from 'react-native';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Provider as PaperProvider, Button, Text } from 'react-native-paper';
 import PropTypes from 'prop-types';
 
